3b95971f3f2efce84dfc8c20a66a460947b44386
Davide Rizzi <d.rizzi@novalabs.io>
2018-03-12 11:36:20 +0100

    Uncrustified

--- include/pid_ie.hpp
+++ include/pid_ie.hpp
@@ -1,64 +1,108 @@
-#ifndef _PID_EI_EI_HPP_
-#define _PID_EI_EI_HPP_
+#pragma once
 
 #include <float.h>
 
-namespace core
+namespace core {
+namespace pid_ie {
+class PID_IE
 {
-namespace pid_ie
-{
-    
-class PID_IE {
 private:
-	// Parameters
-	float _k;
-	float _a;
-	float _b1;
-	float _b2;
-	float _min;
-	float _max;
+    // Parameters
+    float _k;
+    float _a;
+    float _b1;
+    float _b2;
+    float _min;
+    float _max;
 
-	// Status
-	float _ui;
-	float _ud;
-	float _errorOld;
-	float _outputOld;
+    // Status
+    float _ui;
+    float _ud;
+    float _errorOld;
+    float _outputOld;
 
-	// Inputs
-	bool _auto;
-	bool _fHigh;
-	bool _fLow;
-	float _control;
-	float _setpoint;
+    // Inputs
+    bool  _auto;
+    bool  _fHigh;
+    bool  _fLow;
+    float _control;
+    float _setpoint;
 
-	// Outputs
-	bool _sHigh;
-	bool _sLow;
-	bool _flagsChanged;
+    // Outputs
+    bool _sHigh;
+    bool _sLow;
+    bool _flagsChanged;
 
 public:
-	PID_IE(void);
-	void config(float k, float ti, float td, float ts, float N, float min,
-			float max);
-	void set(float setpoint);
-	void reset(void);
-	float get_setpoint(void);
-	float update(float measure);
-	float getLastOutput();
+    PID_IE(
+        void
+    );
+    void
+    config(
+        float k,
+        float ti,
+        float td,
+        float ts,
+        float N,
+        float min,
+        float max
+    );
 
-	// methods to set extra inputs
-	void setAuto(bool autoMode);
-	void setControl(float control);
-	void setFreezeHigh(bool fHigh);
-	void setFreezeLow(bool fLow);
+    void
+    set(
+        float setpoint
+    );
 
-	// methods to get outputs
-	bool flagsChanged();
-	bool isSaturatedHigh();
-	bool isSaturatedLow();
+    void
+    reset(
+        void
+    );
+
+    float
+    get_setpoint(
+        void
+    );
+
+    float
+    update(
+        float measure
+    );
+
+    float
+    getLastOutput();
+
+
+    // methods to set extra inputs
+    void
+    setAuto(
+        bool autoMode
+    );
+
+    void
+    setControl(
+        float control
+    );
+
+    void
+    setFreezeHigh(
+        bool fHigh
+    );
+
+    void
+    setFreezeLow(
+        bool fLow
+    );
+
+
+    // methods to get outputs
+    bool
+    flagsChanged();
+
+    bool
+    isSaturatedHigh();
+
+    bool
+    isSaturatedLow();
 };
-
 }
 }
-
-#endif /* _PID_EI_EI_HPP_ */
